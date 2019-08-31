269b43f1acefdf0fabf279645b8c9edf7c3405ae
Serhii_Y <dj.ser961@gmail.com>
2019-08-31 20:32:57 +0300

    Added some variables and functions to 5libraryPage.js

---
+++ src/js/1initialHomePage.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const API_KEY = 'be5814d92900bfa53e515c0fd30f3d51';
+
+const refs = {
+  watchButton: document.querySelector('button[data-action="watch-button"]'),
+  queueButton: document.querySelector('button[data-action="queue-button"]')
+}
